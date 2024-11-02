0fe3306a06bdb60caa352161ed31e0a5de4a8bcd
Brijesh Singh <bsingh6636@gmail.com>
2024-11-02 09:58:38 +0545

    check

--- getdata/forward.controller.js
+++ getdata/forward.controller.js
@@ -22,7 +22,7 @@ export const forwardUrl = asyncErrorHandler(async (req, res) => {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 'Accept': 'application/json, text/plain, */*',
-                'Referer': 'https://www.swiggy.com/',
+                // 'Referer': 'https://www.swiggy.com/',
             },
         });
 
